b2e77901b4b5835be099a82326cfc0ed9c066d78
Benjamin Catouillard <catouillard.benjamin@gmail.com>
2020-03-31 23:10:31 +0200

    Fix(routing): fix route missing exact path

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 import Login from "./Login/Login.container";
 import Datagrid from "./Datagrid/Datagrid.container";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => (
   <div className="App">
     <Router>
-      <Route path="/" component={Login}></Route>
-      <Route path="/datagrid" component={Datagrid}></Route>
+      <Switch>
+        <Route exact path="/" component={Login}></Route>
+        <Route path="/datagrid" component={Datagrid}></Route>
+      </Switch>
     </Router>
   </div>
 );
